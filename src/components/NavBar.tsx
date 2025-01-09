0f47f0afb1e70ee636dd0e76fb629cb826092210
Pramod Lakmal <pramodlakmalofficial@gmail.com>
2025-01-10 04:22:10 +0530

    Responsive Menu Design

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+const NavBar = () => {
+  return (
+    <div className='flex items-center justify-between p-4'>
+        {/* SEARCH BAR */}
+        <div className=""></div>
+        {/* ICONS AND USER */}
+        <div className=""></div>
+    </div>
+  )
+}
+
+export default NavBar
